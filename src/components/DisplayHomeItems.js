5afc790854b76386cbfdf118acacd7d89f5973a1
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-13 18:48:59 +0530

    Update dispay home items

--- src/components/DisplayHomeItems.js
+++ src/components/DisplayHomeItems.js
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux';
+import { IoIosArrowForward } from 'react-icons/io';
+import style from '../Style/DisplayHomeItems.module.css';
 
 const DisplayHomeItems = () => {
   const { coins, status } = useSelector((store) => store.coins);
@@ -13,23 +15,30 @@ const DisplayHomeItems = () => {
   }
 
   return (
-    <>
+    <div className={style.container}>
       <div>{outPut}</div>
-      {
+      <input type="search" placeholder="Search" />
+      <div className={style.itemWrapp}>
+        {
         coins.map((item) => (
-          <div key={item.id}>
-            <div>click</div>
-            <img src={item.image} alt={item.name} />
-            <div>{item.name}</div>
+          <div key={item.id} className={style.itemContainer}>
             <div>
-              $
-              {item.current_price}
+              <div><IoIosArrowForward /></div>
+              <img className={style.img} src={item.image} alt={item.name} />
             </div>
-            <div>{item.price_change_percentage_24}</div>
+            <div>
+              <div>{item.name}</div>
+              <div>
+                $
+                {item.current_price}
+              </div>
+              <div>{item.price_change_percentage_24}</div>
+            </div>
           </div>
         ))
     }
-    </>
+      </div>
+    </div>
   );
 };
 
