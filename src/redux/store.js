4f1f5587dbcdcdf04d06a663c7df8c34a1252382
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-14 20:00:05 +0530

    Add detailReducer in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import coinReducer from './HomePage/HomePageSlice';
+import detailReducer from './DetailsPage/DetailsPageSlice';
 
 const store = configureStore({
   reducer: {
     coins: coinReducer,
+    details: detailReducer,
   },
 });
 
