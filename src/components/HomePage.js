b726b06f209edf6fbcbdfacec37bed154d92a3ae
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-14 19:59:35 +0530

    Add herosection img

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { AiOutlineSetting } from 'react-icons/ai';
 import { BiMicrophone } from 'react-icons/bi';
+import { useSelector } from 'react-redux';
 import DisplayHomeItems from './DisplayHomeItems';
-import logo from '../assets/logo.JPG';
+import logo from '../assets/logo.png';
 import style from '../Style/HomePage.module.css';
+import Footer from './Footer';
+import chart from '../assets/chart.png';
 
-const HomePage = () => (
-  <div className={style.container}>
-    <div className={style.nav}>
-      <div>
-        <img src={logo} alt="logo" width="100px" />
+const HomePage = () => {
+  const { coins } = useSelector((store) => store.coins);
+  return (
+    <div className={style.container}>
+      <div className={style.nav}>
+        <div>
+          <img src={logo} alt="logo" width="100px" />
+        </div>
+        <div className={style.icons}>
+          <div><BiMicrophone /></div>
+          <div><AiOutlineSetting /></div>
+        </div>
       </div>
-      <div className={style.icons}>
-        <div><BiMicrophone /></div>
-        <div><AiOutlineSetting /></div>
+      <div className={style.heroSection}>
+        <img src={chart} alt="chart" />
+        <div className={style.heroSectionText}>
+          <div>Crypto Vault</div>
+          <div className={style.ptag}>
+            {coins.length}
+            {' '}
+            Coins Listed
+          </div>
+        </div>
       </div>
+      <DisplayHomeItems />
+      <Footer />
     </div>
-    <div className={style.heroSection}>
-      <div className={style.heroSectionText}>
-        <div>Crypto Vault</div>
-        <div className={style.ptag}>40 coins</div>
-      </div>
-    </div>
-    <DisplayHomeItems />
-  </div>
-);
-
+  );
+};
 export default HomePage;
